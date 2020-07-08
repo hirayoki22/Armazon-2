137bae501cd36839444d74023ec4ef7843ce825e
Adison Pena <hirayoki.nova@gmail.com>
2020-07-07 20:20:26 -0400

    m

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,6 +10,12 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const url = 'http://127.0.0.1/market-api/products.php';
+
+    fetch(url)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.log(err.message()));
   }
 
 }
