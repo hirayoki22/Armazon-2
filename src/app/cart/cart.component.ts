376564310af13d1eb24ddd4fec85eb0faa1f5be7
Adison Pena <hirayoki.nova@gmail.com>
2020-07-08 01:41:38 -0400

    m

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,45 +3,41 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 import { Product } from '../product.model';
 import { Observable } from 'rxjs';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss'],
-  animations: [
-    trigger('showCart', [
-      state('close', style({
-        visibility: 'hidden',
-        transform: 'translateX(100%)'
-      })),
-      state('open', style({
-        visibility: 'visible',
-        transform: 'translateX(0)'
-      })),
-      transition('close <=> open', animate('0.3s ease'))
-    ])
-  ]
+  styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
   cartItems: Product[] = [];
-  viewCart: Observable<boolean>;
+  viewCart: boolean;
 
   constructor(private cs: CartService) { }
 
   ngOnInit(): void {
-    this.viewCart = this.cs.cartViewState$;
 
-    this.viewCart.subscribe(state => {
+    this.cs.cartViewState$.subscribe(state => {
       if (state) {
+        this.viewCart = state;
         document.body.classList.add('active-overlay');
       }
     });
   }
 
   onClose(): void {
-    this.cs.changeCartViewState(false);
-    document.body.classList.remove('active-overlay');
+    const overlay = document.querySelector('.overlay');
+    const section = document.querySelector('.cart-container');
+
+    section.classList.add('slide-out');
+    overlay.classList.add('hide');
+
+    setTimeout(() => {
+      this.viewCart = false;
+      overlay.classList.remove('hide');
+      section.classList.remove('slide-out');
+    }, 400);
   }
 
   textClipper(text: string, limit: number): string {
