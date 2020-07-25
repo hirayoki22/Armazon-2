36e0baa5594699fc77d7fbc54b3e7c2bada24f85
Adison Pena <hirayoki.nova@gmail.com>
2020-07-25 13:00:55 -0400

    m

--- src/app/shared/aside-panel/aside-panel.component.ts
+++ src/app/shared/aside-panel/aside-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild, ViewChildren, ContentChild, ContentChildren, ElementRef, QueryList } from '@angular/core';
+import { Input, Output } from '@angular/core';
 
 @Component({
   selector: 'aside-panel',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aside-panel.component.scss']
 })
 export class AsidePanelComponent implements OnInit {
+  @Input() heading: string = '';
+  @Input() showPanel: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    document.body.classList.add('active-overlay');
+
+    window.onkeyup = (e: KeyboardEvent) => {
+      if (e.key == 'Escape') { this.onClose(); }
+    }
+  }
+
+  onClose(): void {
+    const overlay = document.querySelector('.overlay');
+    const section = document.querySelector('.panel');
+
+    section.classList.add('slide-out');
+    overlay.classList.add('hide');
+    window.onkeyup = null;
+
+    setTimeout(() => {
+      document.body.classList.remove('active-overlay');
+      overlay.classList.remove('hide');
+      section.classList.remove('slide-out');
+      this.showPanel = false;
+    }, 400);
+  }
+
+  overlayClick(e: MouseEvent): void {
+    if (e.target == e.currentTarget) { this.onClose(); }
   }
 
 }
