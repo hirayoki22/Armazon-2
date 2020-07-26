70fd4642af5f5568743180624534c74b997e592d
Adison Pena <hirayoki.nova@gmail.com>
2020-07-25 23:19:56 -0400

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,35 +9,6 @@ export class AppComponent implements OnInit {
   title = 'marketplace';
 
   ngOnInit(): void {
-    // this.getShoppingCart();
-    // this.addToCart();
-  }
-
-  getShoppingCart(): void {
-    const formData = new FormData();
-    formData.append('userId', '1');
-
-    fetch('http://127.0.0.1/market-api/shopping-cart.php', {
-      method: 'POST',
-      body: formData
-    })
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.log(err.message));
-  }
-
-  addToCart(): void {
-    const formData = new FormData();
-    formData.append('userId', '1');
-    formData.append('productId', '8');
-    formData.append('quantity', '1');
-
-    fetch('http://127.0.0.1/market-api/shopping-cart.php', {
-      method: 'POST',
-      body: formData
-    })
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.log(err.message));
+    
   }
 }
