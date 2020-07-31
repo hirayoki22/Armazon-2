f54801e1770ac510c328e1af8f4fe07013c9a003
Adison Pena <hirayoki.nova@gmail.com>
2020-07-31 02:11:52 -0400

    m

--- src/app/product-details/slider/lightbox/lightbox.component.ts
+++ src/app/product-details/slider/lightbox/lightbox.component.ts
@@ -3,7 +3,6 @@ import { ViewChild, ElementRef } from '@angular/core';
 import { Input } from '@angular/core';
 
 import { LightboxService } from './lightbox.service';
-import { LightboxData } from './lightbox.model';
 
 @Component({
   selector: 'lightbox',
@@ -16,6 +15,8 @@ export class LightboxComponent implements AfterViewInit {
   @Input() images: string[];
   currentImage: number = 0;
   openLightbox: boolean = false;
+  viewFullImage: boolean = false;
+
 
   constructor(private ls: LightboxService) { }
 
@@ -61,54 +62,74 @@ export class LightboxComponent implements AfterViewInit {
   }
 
   onRangeClick(e: MouseEvent): void {
-    const range      = <HTMLElement>(e.currentTarget);
-    const indicator  = this.rangeIndicator.nativeElement;
-    const width      = indicator.getBoundingClientRect().width;
-    const left       = indicator.getBoundingClientRect().left;
-    const right      = indicator.getBoundingClientRect().right;
-    const pageOffset = range.getBoundingClientRect().left;
+    const indicator = this.rangeIndicator.nativeElement;
+    const width     = indicator.getBoundingClientRect().width;
+    const left      = indicator.getBoundingClientRect().left;
+    const right     = indicator.getBoundingClientRect().right;
+    const offset    = indicator.parentElement.offsetLeft;
     
     if (e.clientX < left || e.clientX > right) {
-      const position = Math.floor((e.clientX - pageOffset) / width);
+      const position = Math.floor((e.clientX - offset) / width);
 
       this.currentImage = position > 0 ? position : 0;
       this.ls.openLightbox({index: this.currentImage, scrollBehavior: 'smooth'});
     }
   }
 
-  viewFullImage: boolean = false;
-
   onImageClick(image: HTMLImageElement): void {
-    this.viewFullImage = true;
+    this.viewFullImage = !this.viewFullImage;
 
-    image.style.width  = `${image.naturalWidth / 2}px`;
-    image.style.height = `${image.naturalHeight / 2}px`;
+    if (this.viewFullImage) {
+      image.style.width  = `${image.naturalWidth / 2}px`;
+      image.style.height = `${image.naturalHeight / 2}px`;
+
+      image.style.left = '50%';
+      image.style.top = '50%';
+      image.style.transform = 'translate(-50%, -50%)';
+    } else {
+      image.style.width  = '100%';
+      image.style.height = '100%';
+
+      image.style.left = 'unset';
+      image.style.top = 'unset';
+      image.style.transform = 'none';
+    }
   }
 
   onMouseMove(e: MouseEvent): void {
     if (!this.viewFullImage) { return; }
 
-    const frame = <HTMLElement>e.currentTarget;
-    const image = <HTMLImageElement>frame.firstElementChild;
-    const frameLeft  = frame.getBoundingClientRect().left;
-    const frameTop   = frame.getBoundingClientRect().top;
+    const image = <HTMLImageElement>e.currentTarget;
+    const frame = image.parentElement;
 
-    const posX = (e.clientX - frameLeft) * -1;
-    const posY = (e.clientY - frameTop) * -1;
+    const left   = image.getBoundingClientRect().left;
+    const top    = image.getBoundingClientRect().top;
+    const right  = image.getBoundingClientRect().right;
+    const bottom = image.getBoundingClientRect().bottom;
 
-    // image.style.transform = `translateX(${-300}px)`;
-    // image.style.transform = `translateY(${posY}px)`;
+    // let pathLeft = 
+
+    // let pathTop = 
+
+    // image.style.left = `${pathLeft}px`;
+    // image.style.top = `${pathTop}px`;  
+    // console.log({left: left, top: top});
     
-    // frame.scrollTo({ top: e.clientX, left: e.clientY, behavior: 'smooth' });
+    // if (e.clientX >= frameLeft && e.clientX <= frame.clientWidth) {
+      
+    //   console.log((e.clientX - frameLeft) - left);
+    // }
+  
+    // console.log({left: left, right: right, top: top, bottom: bottom});
     
-    const left = ele.getBoundingClientRect().left - 1;
-    const top = ele.getBoundingClientRect().top - 1;
-    const right = ele.getBoundingClientRect().right + 1;
-    const bottom = ele.getBoundingClientRect().bottom + 1;
-
-    if (image.getBoundingClientRect().left > 0) {
-      image.style.left = `${(e.clientX - left) - image.getBoundingClientRect().left}px`;
-    }
+    // image.style.left = `${e.clientX - 308}px`;
+    const frameRects = frame.getBoundingClientRect();
+    
+    image.style.left = `${e.offsetX + 80}px`;
+    image.style.top = `${e.offsetY + 80}px`;
+    
+  
+    console.log(e.offsetX);
   }
 
   onClose(): void {
