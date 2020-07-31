696fb58b99fe2ca3d8ab4c9abe0ba9009609e21b
Adison Pena <hirayoki.nova@gmail.com>
2020-07-31 02:18:24 -0400

    m

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -10,6 +10,22 @@ export class AdminPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let container = <HTMLElement>document.querySelector("#jamf-container");
+    let mover = <HTMLElement>document.querySelector("#jamf-mover");
+
+    container.addEventListener("mousemove", (e: MouseEvent) => {
+      mover.style.backgroundPositionX = -e.offsetX * 1.8 + "px";
+      mover.style.backgroundPositionY = -e.offsetY + 80 + "px";
+    });
+
+    container.addEventListener("mouseenter", function() {
+      
+      setTimeout(function() {
+        mover.classList.add("no-more-slidey");
+        container.removeEventListener("mouseenter", this);
+      }, 250);
+      
+    });
   }
 
 }
