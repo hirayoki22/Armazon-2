4dd00198ce34c337f01681b26f4a315f896524f9
Adison Pena <hirayoki.nova@gmail.com>
2020-08-02 15:57:45 -0400

    m

--- src/app/product-ratings/review-form/review-form.component.ts
+++ src/app/product-ratings/review-form/review-form.component.ts
@@ -18,7 +18,7 @@ export class ReviewFormComponent implements OnInit {
   showForm: boolean = false;
   isLoading: boolean = true;
   reviewSubmitted: boolean = false;
-  userReviewExists: boolean = true;
+  userReviewExists: boolean = false;
 
   constructor(
     private ps: ProductService,
