86a333510a2251281529f04b2583141a48c75753
Adison Pena <hirayoki.nova@gmail.com>
2020-08-01 23:39:04 -0400

    m

--- src/app/product-ratings/review-form/review-form.component.ts
+++ src/app/product-ratings/review-form/review-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { ReviewFormService } from './review-form.service';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/product.service';
+
+interface QuickInfo { productName: string; image: string; }
 
 @Component({
   selector: 'review-form',
@@ -7,15 +13,46 @@ import { ReviewFormService } from './review-form.service';
   styleUrls: ['./review-form.component.scss']
 })
 export class ReviewFormComponent implements OnInit {
+  product: QuickInfo;
+  reviewForm: FormGroup;
   showForm: boolean = false;
   isLoading: boolean = false;
 
-  constructor(private rf: ReviewFormService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private ps: ProductService,
+    private rf: ReviewFormService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
     this.rf.$formState.subscribe(() => {
+      this.reviewForm = this.initiReviewForm();
       this.showForm = true;
+
+      this.getProduct();
     });
+  }
+
+  getProduct(): void {
+    const productId = +this.route.snapshot.paramMap.get('id');
+    
+    console.log(productId);
+    this.route.paramMap.subscribe(params => {
+      console.log(params);
+    });
+  }
+
+  private initiReviewForm(): FormGroup {
+    return this.fb.group({
+      overallRating: [ 0, Validators.required ],
+      headline: [ null ],
+      review: [ null, Validators.required ]
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.reviewForm.value);
   }
 
   onClose(): void {
