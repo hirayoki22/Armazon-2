938409f346a60ecea47e4017c69e719753e27583
Adison Pena <hirayoki.nova@gmail.com>
2020-08-04 01:01:02 -0400

    m

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, map, catchError, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private URL1  = 'http://127.0.0.1/market-api/user-login.php';
+  private URL2 = 'http://127.0.0.1/market-api/user-signup.php';
+  private URL3 = 'http://127.0.0.1/market-api/user-logout.php';
+  private URL4 = 'http://127.0.0.1/market-api/user-auth.php';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+
+
+  private errorHandler(err: HttpErrorResponse) {
+    let error = '';
+
+    if (err.error instanceof ErrorEvent) {
+      error = err.error.message;
+    } else {
+      error = err.error;
+    }
+    return throwError(error);
+  }
+
 }
