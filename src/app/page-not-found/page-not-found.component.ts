d50fd51c143bcd9026ec3166471c472f45bc71fe
Adison Pena <hirayoki.nova@gmail.com>
2020-08-03 21:47:49 -0400

    m

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
+  loginForm: FormGroup;
+  loginMessage: { loginSuccess: boolean, message: string };
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.initLoginForm();
   }
 
+  initLoginForm(): FormGroup {
+    return this.fb.group({
+      username: [ null, Validators.required ],
+      password: [ null, Validators.required ],
+    });
+  }
+
+  get username(): string {
+    return this.loginForm.get('username').value.trim();
+  }
+
+  get password(): string {
+    return this.loginForm.get('password').value.trim();
+  }
+
+  onSubmit(): void {
+    const URL = 'http://127.0.0.1/market-api/test.php';
+
+    const formData = new FormData();
+    formData.append('password', this.password);
+    formData.append('username', this.username);
+    
+    fetch(URL, { method: 'POST', body: formData })
+    .then(res => res.json())
+    .then(res => this.loginMessage = res)
+    .catch(err => err.getMessage);
+  }
 }
