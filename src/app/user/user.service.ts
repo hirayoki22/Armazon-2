6954068f3fa9b9d720c60d5f3b8b01a438b4466e
Adison Pena <hirayoki.nova@gmail.com>
2020-08-07 15:44:35 -0400

    m

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -24,7 +24,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class UserService {
-  private URL1  = 'http://127.0.0.1/market-api/user.php';
+  private URL1 = 'http://127.0.0.1/market-api/user.php';
   private URL2 = 'http://127.0.0.1/market-api/user-login.php';
   private URL3 = 'http://127.0.0.1/market-api/user-signup.php';
   private URL4 = 'http://127.0.0.1/market-api/user-logout.php';
