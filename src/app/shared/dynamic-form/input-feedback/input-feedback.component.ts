4c54eeecf07cc1f7b9978d69325dae4fa9451ce4
Adison Pena <hirayoki.nova@gmail.com>
2020-08-09 15:44:21 -0400

    m

--- src/app/shared/dynamic-form/input-feedback/input-feedback.component.ts
+++ src/app/shared/dynamic-form/input-feedback/input-feedback.component.ts
@@ -17,45 +17,10 @@ export class InputFeedbackComponent implements OnInit {
   get label(): string {
     return this.field?.fieldLabel.toLowerCase();
   }
-
-  feedbacks: Feedback[];
   
   constructor() { }
 
   ngOnInit(): void {
-    this.control.valueChanges.subscribe(() => {
-      console.log(Object.keys(this.control.errors)[0]);
-      // this.feedbacks = this.initFeedbacks();
-    });
   }
 
-  initFeedbacks(): Feedback[] {
-    return [
-      { 
-        condition: this.control?.errors.required,
-        message: `Enter your ${this.label}`
-      },
-      { 
-        condition: this.control?.errors.pattern,
-        message: `Invalid ${this.label} format`
-      },
-      { 
-        condition: this.control?.errors.minlength,
-        message: `Cannot be less than 
-        ${this.control?.errors?.minlength?.requiredLength} characters`
-      },
-      { 
-        condition: this.control?.errors.exists,
-        message: `This ${this.label} is already in use`
-      },
-      { 
-        condition: this.control?.value && this.control?.errors?.password,
-        message: `This ${this.label} is not secure`
-      },
-      // { 
-      //   condition: this.control.dirty && this.field?.customFeedback?.condition,
-      //   message: this.field?.customFeedback?.message
-      // }
-    ];
-  }
 }
