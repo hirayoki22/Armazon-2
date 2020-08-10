d28768da999d0d428b43382c5b82d3621d1bd3af
Adison Pena <hirayoki.nova@gmail.com>
2020-08-10 18:28:04 -0400

    m

--- src/app/product-ratings/review-form/review-form.component.ts
+++ src/app/product-ratings/review-form/review-form.component.ts
@@ -47,7 +47,7 @@ export class ReviewFormComponent implements OnInit {
   onSubmit(form?: FormGroup): void {
     const review: NewReview = {
       productId: this.product.productId,
-      userId: 1,
+      userId: 8,
       headline: form.get('headline').value.trim(),
       review: form.get('review').value.trim(),
       rating: form.get('rating').value      
