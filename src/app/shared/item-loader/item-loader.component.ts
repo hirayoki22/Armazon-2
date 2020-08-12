a7fca4f0e6481e88b3ab443f27458c93ede23081
Adison Pena <hirayoki.nova@gmail.com>
2020-08-12 15:15:31 -0400

    m

--- src/app/shared/item-loader/item-loader.component.ts
+++ src/app/shared/item-loader/item-loader.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'item-loader',
   templateUrl: './item-loader.component.html',
-  styleUrls: ['./item-loader.component.scss']
+  styleUrls: ['./item-loader.component.scss'],
+  animations: [
+    trigger('showSpinner', [
+      state('hide', style({
+        visibility: 'hidden',
+        opacity: 0
+      })),
+      state('show', style({
+        visibility: 'visible',
+        opacity: 1
+      })),
+      transition('hide <=> show', animate('0.4s ease'))
+    ])
+  ]
 })
 export class ItemLoaderComponent implements OnInit {
-  fakeItemsCount: any = Array(5);
+  fakeItemsCount: any = Array(6);
   @Input() isLoading = false;
 
   constructor() { }
