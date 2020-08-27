b781763768315393ebca175b25812549124020be
Adison Pena <hirayoki.nova@gmail.com>
2020-08-26 21:38:33 -0400

    m

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,7 +9,7 @@ import { CartService } from '../product/services/cart.service';
 })
 export class NavbarComponent implements OnInit {
   itemCount: number | string;
-  showSearchbox: boolean = true;
+  showSearchbox: boolean = false;
 
   constructor(private cs: CartService) { }
 
