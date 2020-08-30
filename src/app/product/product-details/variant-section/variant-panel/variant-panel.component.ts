021b9c3a292560097e2ea0b9b6ee5d29fbb61b47
Adison Pena <hirayoki.nova@gmail.com>
2020-08-29 22:44:09 -0400

    m

--- src/app/product/product-details/variant-section/variant-panel/variant-panel.component.ts
+++ src/app/product/product-details/variant-section/variant-panel/variant-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 import { VariantPanelService } from 'src/app/product/services/variant-panel.service';
 import { VariantOption } from '../variant-section.component';
@@ -10,23 +11,34 @@ interface PanelData { option: VariantOption, activeVariant: number }
   templateUrl: './variant-panel.component.html',
   styleUrls: ['./variant-panel.component.scss']
 })
-export class VariantPanelComponent implements OnInit {
+export class VariantPanelComponent implements AfterViewInit {
+  @ViewChildren('optionBtn') optionBtns: QueryList<ElementRef<HTMLElement>>; 
   option: VariantOption;
-  showPanel: boolean = false;
   activeVariant: number = 0;
 
   constructor(private panelService: VariantPanelService) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.panelService.$panelState.subscribe((data: PanelData) => {
       this.option = data.option;
       this.activeVariant = data.activeVariant;
-      this.showPanel = true;
+      
+      setTimeout(() =>this.scrollToOption(), 60);
     });
   }
 
+  private scrollToOption(): void {
+    const optionBtns = this.optionBtns.toArray()
+    .map(option => option.nativeElement);
+
+    const index = this.option.variants.findIndex(variant => {
+      return variant.variantId == this.activeVariant;
+    });
+
+    optionBtns[index].scrollIntoView({ block: 'center' });
+  }
+
   onClose(): void {
-    this.showPanel = false;
     this.activeVariant = 0;
     this.option = null;
   }
