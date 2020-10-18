d43797a7c1f06447be6abeb6de1246c3679c3d1d
Adison Pena <hirayoki.nova@gmail.com>
2020-10-18 13:32:17 -0400

    Made some changes. Whatever

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  @ViewChild('video') video: ElementRef<HTMLVideoElement>;
+  @ViewChild('progressBar') progressBar: ElementRef<HTMLElement>;
+  isPlaying: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  onPlaying(video: HTMLVideoElement): void {
+    const bar = this.progressBar.nativeElement;
+    const progress = video.currentTime / video.duration * 100;
+
+    this.isPlaying = !video.paused;
+    bar.style.width = `${progress}%`;
+  }
+
+  playPauseVideo(): void {
+    const video = this.video.nativeElement;
+    this.isPlaying = !this.isPlaying;
     
+    if (!video.paused) {
+      video.pause();
+    } else {
+      video.play();
+    }
   }
 }
