ad6a0da0fdfdfb9387fc589dbe97cf0ad408e41d
Adison Pena <hirayoki.nova@gmail.com>
2020-12-10 10:11:05 -0400

    Removed unnecessary cluter

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit} from '@angular/core';
-import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,32 +6,10 @@ import { ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  @ViewChild('video') video: ElementRef<HTMLVideoElement>;
-  @ViewChild('progressBar') progressBar: ElementRef<HTMLElement>;
-  isPlaying: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
 
-  }
-
-  onPlaying(video: HTMLVideoElement): void {
-    const bar = this.progressBar.nativeElement;
-    const progress = video.currentTime / video.duration * 100;
-
-    this.isPlaying = !video.paused;
-    bar.style.width = `${progress}%`;
-  }
-
-  playPauseVideo(): void {
-    const video = this.video.nativeElement;
-    this.isPlaying = !this.isPlaying;
-    
-    if (!video.paused) {
-      video.pause();
-    } else {
-      video.play();
-    }
   }
 }
