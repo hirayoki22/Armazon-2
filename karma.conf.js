7c73655e73a536e4859e7e4fd183992ff3b6623c
Adison Pena <hirayoki.nova@gmail.com>
2020-12-14 14:37:41 -0400

    m

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/marketplace'),
+      dir: require('path').join(__dirname, './coverage/Armazon 2'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
